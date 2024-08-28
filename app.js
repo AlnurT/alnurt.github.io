f9f64263dbb3443131d7b2c168ad2b57e6e8bd87
Alnur <87542661+AlnurT@users.noreply.github.com>
2024-08-28 21:02:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let tg = window.Telegram.WebApp;
 let info = document.getElementById("info_btn");
 let rules = document.getElementById("rules_btn");
+let address = document.getElementById("address");
+let tel = document.getElementById("tel");
 
 info.addEventListener("click", () => {
     document.getElementById("info").style.display = "block";
@@ -10,4 +12,13 @@ info.addEventListener("click", () => {
 rules.addEventListener("click", () => {
     document.getElementById("info").style.display = "none";
     document.getElementById("rules").style.display = "block";
+});
+
+address.addEventListener("click", () => {
+    tg.openLink("https://yandex.ru/maps/org/klub_voinskogo_iskusstva_teskao_bely_volk/1694603185/?ll=30.339000%2C59.967488&z=18");
+});
+
+tel.addEventListener("click", () => {
+
+    tg.showAlert("Телефон скопирован")
 });
