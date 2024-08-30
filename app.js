818d50ccf2c7e7945b83d536a140c119e08faff0
Alnur <87542661+AlnurT@users.noreply.github.com>
2024-08-30 16:45:14 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let info = document.getElementById("info_btn");
 let rules = document.getElementById("rules_btn");
 let address = document.getElementById("address");
 let tel = document.getElementById("tel");
-let count = document.getElementById("count");
 
 
 info.addEventListener("click", () => {
@@ -20,12 +19,6 @@ rules.addEventListener("click", () => {
     document.getElementById("rules_btn").style.display = "none";
 });
 
-count.addEventListener("click", () => {
-    let pl = document.getElementById("players").value;
-    document.getElementById("price").value =
-        (pl < 8 || pl > 14) ? "ошибка" : Math.round(3500 / pl);
-});
-
 address.addEventListener("click", () => {
     tg.openLink("https://yandex.ru/maps/org/klub_voinskogo_iskusstva_teskao_bely_volk/1694603185/?ll=30.339000%2C59.967488&z=18");
 });
@@ -34,3 +27,8 @@ tel.addEventListener("click", () => {
     navigator.clipboard.writeText('+79006200637');
     tg.showAlert("Телефон скопирован");
 });
+
+players.oninput = function () {
+    let pl = players.value
+    price.innerHTML = (pl < 8 || pl > 14) ? "ошибка" : Math.round(3500 / pl);
+}
