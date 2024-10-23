97bb83103266b74a2abed8dc45367916dbc2d876
Alnur <87542661+AlnurT@users.noreply.github.com>
2024-10-23 13:51:19 +0300

    Add rules and new phone number

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let tg = window.Telegram.WebApp;
 let info = document.getElementById("info_btn");
 let rules = document.getElementById("rules_btn");
 let address = document.getElementById("address");
-let tel = document.getElementById("tel");
+let tel_admin = document.getElementById("tel_admin");
+let tel_capt = document.getElementById("tel_capt");
+let players;
 
 
 info.addEventListener("click", () => {
@@ -23,12 +25,18 @@ address.addEventListener("click", () => {
     tg.openLink("https://yandex.ru/maps/org/klub_voinskogo_iskusstva_teskao_bely_volk/1694603185/?ll=30.339000%2C59.967488&z=18");
 });
 
-tel.addEventListener("click", () => {
+tel_admin.addEventListener("click", () => {
+    navigator.clipboard.writeText('+79119673502');
+    tg.showAlert("Телефон скопирован");
+});
+
+tel_capt.addEventListener("click", () => {
     navigator.clipboard.writeText('+79006200637');
     tg.showAlert("Телефон скопирован");
 });
 
 players.oninput = function () {
     let pl = players.value
-    price.innerHTML = (pl < 8 || pl > 14) ? "ошибка" : Math.round(3500 / pl);
+    let price;
+    price.innerHTML = pl < 8 ? "ошибка" : Math.round(3500 / pl);
 }
